48d1b1a8d697913f3ae1c837795160ace37edafa
B-a11yt67689 <am225430@gmail.com>
2026-03-10 17:41:08 +0500

    3-5

--- index.js
+++ index.js
@@ -3,19 +3,49 @@
 function animaster() {
     const animaster = {
         fadeIn(element, duration) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.classList.remove('hide');
             element.classList.add('show');
         },
+
+        fadeOut(element, duration) {
+            element.style.transitionDuration = `${duration}ms`;
+            element.classList.remove('show');
+            element.classList.add('hide');
+        },
+
         move(element, duration, translation) {
             element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(translation, null);
         },
+
         scale(element, duration, ratio) {
-            element.style.transitionDuration =  `${duration}ms`;
+            element.style.transitionDuration = `${duration}ms`;
             element.style.transform = getTransform(null, ratio);
+        },
+
+        moveAndHide(element, duration) {
+            const moveTime = duration * 2 / 5;
+            const fadeTime = duration * 3 / 5;
+
+            this.move(element, moveTime, {x: 100, y: 20});
+
+            setTimeout(() => {
+                this.fadeOut(element, fadeTime);
+            }, moveTime);
+        },
+
+        showAndHide(element, duration) {
+            const part = duration / 3;
+
+            this.fadeIn(element, part);
+
+            setTimeout(() => {
+                this.fadeOut(element, part);
+            }, part * 2);
         }
     }
+
     return animaster;
 }
 
